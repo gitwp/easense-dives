290632afd24578c8b275ab021db4beb15600afd1
easense2011 <easense2011@gmail.com>
2012-05-20 01:22:23 +0000

    Add a new class to implement some methods of Math and also some test cases.

---
+++ Foobar/src/test/java/org/easense/MathUtilTest.java
@@ -0,0 +1,11 @@
+package org.easense;
+
+import org.junit.Test;
+
+public class MathUtilTest {
+	
+	@Test
+	public void findMax() {
+		System.out.println(MathUtil.findMax(12, 1, 3 , 9, 128));
+	}
+}
